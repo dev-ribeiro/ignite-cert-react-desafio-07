50be781b9e67b1f8aa8012eea2567e757d91bfad
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-04-20 12:09:47 -0300

    fix: adjust project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,28 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
+interface ImagesResponseType {
+  after: string;
+  data: Image[];
+}
+
+async function fetchImages({ pageParam = null }): Promise<ImagesResponseType> {
+  const { data } = await api('/api/images', {
+    params: {
+      after: pageParam,
+    },
+  });
+  return data;
+}
+
 export default function Home(): JSX.Element {
   const {
     data,
@@ -16,20 +38,25 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastpage => lastpage?.after || null,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    const formatted = data?.pages.flatMap(imageData => {
+      return imageData.data.flat();
+    });
+
+    return formatted;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading && !isError) {
+    return <Loading />;
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (!isLoading && isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +64,15 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+            mt="6"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
